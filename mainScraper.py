19d34ef18da2cb9c835f5de56286ea557857a34d
Nayan ansh <nayanansh@gmail.com>
2020-08-14 22:00:55 +0530

    connect to amazon
    sample link -https://www.amazon.in/s?k=ryzen+5+3600&crid=AYWQTVXD0GWO&sprefix=ryzen+5+3600%2Caps%2C307&ref=nb_sb_ss_organic-diversity_1_12

--- mainScraper.py
+++ mainScraper.py
@@ -1,6 +1,13 @@
 import requests
-
+from bs4 import BeautifulSoup
 RAW_URL = "https://www.amazon.in/AMD-Ryzen-3600-Processor-100000031BOX/dp/B07STGGQ18/ref=sr_1_4?dchild=1&keywords=3600&qid=1596975477&sr=8-4"
 
-req =  requests.get(RAW_URL)
-print(req.text)
+#Amazon is denying service(error code 503) without user agent
+req =  requests.get(RAW_URL,headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"})
+
+if not(req.status_code == 200):
+    print("Something is wrong - \n error code - \n ",req.status_code)
+    SystemExit()
+
+soup =  BeautifulSoup(req.content,"html.parser")
+
