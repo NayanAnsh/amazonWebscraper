a70c58ba502ea216ef2ad86a1b54ad593e003d0d
Nayan ansh <nayanansh@gmail.com>
2020-08-15 17:18:34 +0530

    Database + plotting initialized
    Need to beautify code little bit

--- mainScraper.py
+++ mainScraper.py
@@ -1,14 +1,18 @@
 import requests
+import sqlite3 as sql
+import os
+from datetime import date
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as pyplot
 RAW_URL = "https://www.amazon.in/AMD-Ryzen-3600-Processor-100000031BOX/dp/B07STGGQ18/ref=sr_1_4?dchild=1&keywords=3600&qid=1596975477&sr=8-4"
-
-
+FILEPATH =  os.getcwd()
+today = str(date.today())
 
 
 
 
 def makeConnection():
-        #Amazon is denying service(error code 503) without user agent
+        #Amazon is denying service(error code 503) without user agent Header
     req =  requests.get(RAW_URL,headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"})
     
     if not(req.status_code == 200):
@@ -31,10 +35,65 @@ def getProductName(bs4soup):
     return productName
 req = makeConnection()
 soup =  BeautifulSoup(req.content,"html.parser")
-price = getPrice(soup)
+productPrice = getPrice(soup)
 
 productName =getProductName(soup)
-print(productName)
-  
+productNameShort =  productName[0:45]
+productNameShort =  productNameShort[0:productNameShort.rindex(" ")] #removes incomplete words
+
+
+tableName = productNameShort.replace(" ","_")
+table = tableName+"(date,price)"
+values = '("'+ today +'" , "'+str(productPrice)+'")'
+
+def executeSql(query):
+    output = ""
+    conn =  sql.connect(FILEPATH +"\database\\"+"AmazonDatabase"+".db")
+    c =  conn.cursor()
+    try:
+        c.execute(query)
+        
+        output = c.fetchall()
+        conn.commit()
+        
+        
+    except sql.OperationalError as e:
+        print(e)
+    except sql.IntegrityError as e:
+        print("Entry with this date has been already made, error message -\n",e)
+    conn.close()
+    return output
+
+
+
+def createDB():
+    executeSql("CREATE TABLE "+tableName+"(date PRIMARY KEY,price)")
+def insertDB(values):
+    executeSql("INSERT INTO "+table+" VALUES "+values)
+def searchDB(columnName = "*",where = ""):
+    if where  == "":
+        return executeSql('SELECT ' +columnName+ ' FROM '+tableName)
+    else :
+        return executeSql('SELECT ' +columnName+ ' FROM '+tableName  + ' where '+where)
+
+createDB()
+insertDB(values)
+dateListRaw =  searchDB(columnName = "date")
+dateList = []
+
+for  d in dateListRaw:
+    dateList.append(str(d[0]))
+
+priceListRaw = searchDB(columnName= "price")
+priceList =[]
+for p  in priceListRaw:
+    priceList.append(int(p[0]))
+
+pyplot.plot(dateList,priceList)
+pyplot.show()
+
+
+
+
 
 
---
+++ untitled0.py
@@ -0,0 +1,45 @@
+import sqlite3
+import os
+from datetime import date
+import random
+import matplotlib.pyplot as pyplot
+path = os.getcwd()
+databaseName =  "testDataBase.db"
+conn = sqlite3.connect(path+"/"+databaseName)
+c= conn.cursor()
+
+today = str(date.today())
+try:
+    c.execute("CREATE TABLE sample(date PRIMARY KEY ,text1,text2)")
+except sqlite3.OperationalError as e:
+    print(e)
+q = 'INSERT INTO sample(date,text1,text2) VALUES("'+str(random.randrange(1,100))+'","'+str(random.randrange(1,1000)) +'","sampletext2")'
+try:
+    
+    c.execute(q)
+except :
+    print("unable to enter need unique data")
+conn.commit()
+#c.execute('DELETE FROM sample WHERE text1 = "sampletext1" ' )
+c.execute('SELECT * FROM sample')
+print(c.fetchall())
+
+c.execute('SELECT date FROM sample')
+dateList = c.fetchall()
+c.execute('SELECT text1 FROM sample')
+priceList = c.fetchall()
+
+#pyplot.plot(dateList,priceList)
+#pyplot.show()
+conn.commit()
+conn.close()
+dates = []
+for d in dateList:
+    dates.append(d[0])
+
+prices = []
+for p in priceList:
+    prices.append(int(str(p[0])))
+
+pyplot.plot(dates,prices)
+pyplot.show()
